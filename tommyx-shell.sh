37717f942da5316d9be13ff75a5ba8308a423d70
TommyX <tommyx058@gmail.com>
2024-04-07 16:42:52 -0700

    (no message)

--- tommyx-shell.sh
+++ tommyx-shell.sh
@@ -1,22 +1,43 @@
-## appearance
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
 
-# ZSH_THEME=bira
+export ZSH="$HOME/.oh-my-zsh"
+
+# set zsh theme
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-## extra path
+# extra path
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 
-## plugins
+# oh-my-zsh plugins
+plugins=(
+    git
+    brew
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    zsh-autocomplete
+)
 
-plugins=(git brew zsh-autosuggestions)
+# setup oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+
+# setup powerlevel10k
+[[ ! -f "${0:a:h}/.p10k.zsh" ]] || source "${0:a:h}/.p10k.zsh"
+
+# fzf key bindings
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+[ -f /usr/share/doc/fzf/examples/completions.zsh ] && source /usr/share/doc/fzf/examples/completions.zsh
 
 # fuck
 if type thefuck > /dev/null; then
     eval $(thefuck --alias f)
 fi
-
-## misc configs
 
 # increase number of open files
 alias emacs="ulimit -n 16384 && emacs"
